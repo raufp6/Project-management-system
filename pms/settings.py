f00c50b43822a8cf6382d424301a68fb9e7889e2
raufp6coding <raufp6.coding@gmail.com>
2024-01-09 17:53:22 +0530

    db changed to sqllite to test

--- pms/settings.py
+++ pms/settings.py
@@ -165,17 +165,22 @@ ASGI_APPLICATION = 'pms.asgi.application'
 #         'PORT': os.getenv('DATABASE_PORT'),
 #     }
 # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'pms',
+#         'USER': 'superuser',
+#         'PASSWORD': 'Ra123uof',
+#         'HOST': 'pmsdev.cl4m08c4oonb.eu-north-1.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pms',
-        'USER': 'superuser',
-        'PASSWORD': 'Ra123uof',
-        'HOST': 'pmsdev.cl4m08c4oonb.eu-north-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
-
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
