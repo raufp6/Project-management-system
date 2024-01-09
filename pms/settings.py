0b66ac5323e43004d5f47dd8259fec303f011239
raufp6coding <raufp6.coding@gmail.com>
2024-01-09 17:43:29 +0530

    env varible removed for testing

--- pms/settings.py
+++ pms/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-^oc1zb%h(-+rroekwm#60#mx)(mndpa_f1-+@dq$@*2#7d4x9q"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+# DEBUG = os.getenv('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ["51.20.70.142","localhost","127.0.0.1:8000"]
 
@@ -153,26 +155,26 @@ ASGI_APPLICATION = 'pms.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DATABASE_URL'),
-        'NAME': os.getenv('DATABASE_NAME'), 
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'pms',
-#         'USER': 'superuser',
-#         'PASSWORD': 'Ra123uof',
-#         'HOST': 'pmsdev.cl4m08c4oonb.eu-north-1.rds.amazonaws.com',
-#         'PORT': '5432',
+#         'ENGINE': os.getenv('DATABASE_URL'),
+#         'NAME': os.getenv('DATABASE_NAME'), 
+#         'USER': os.getenv('DATABASE_USER'),
+#         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+#         'HOST': os.getenv('DATABASE_HOST'),
+#         'PORT': os.getenv('DATABASE_PORT'),
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pms',
+        'USER': 'superuser',
+        'PASSWORD': 'Ra123uof',
+        'HOST': 'pmsdev.cl4m08c4oonb.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
