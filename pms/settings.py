2411cd2a31fa54ae12f59031b0a81cb718778e58
raufp6coding <raufp6.coding@gmail.com>
2024-01-10 10:54:24 +0530

    static settings

--- pms/settings.py
+++ pms/settings.py
@@ -222,7 +222,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
